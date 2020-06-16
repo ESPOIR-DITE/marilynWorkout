715c83f03e53c8f3f085c55d1b63554dc4fb7a64
ESPOIR <espoirditekemena@gmail.com>
2020-06-16 21:07:59 +0200

    first commit

---
+++ api/api.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"gopkg.in/resty.v1"
+	"marilynWorkout/config"
+)
+
+/**
+*this is a class that set up the Api address to consume "http://localhost:9099/sts/"
+* Port: 9099 domain: bookstore
+*It also set the type of messaging protocol in our case we will be using JSON
+**/
+const BASE_URL string = "http://localhost:8089/sts/" //connection port
+
+func Rest() *resty.Request {
+	return resty.R().SetAuthToken("").
+		SetHeader("Accept", "application/json").
+		SetHeader("email", "email").
+		SetHeader("site", "site").
+		SetHeader("Content-Type", "application/json")
+}
+
+var JSON = config.ConfigWithCustomTimeFormat
---
+++ go.mod
@@ -0,0 +1,11 @@
+module marilynWorkout
+
+go 1.13
+
+require (
+	github.com/alexedwards/scs/v2 v2.3.1
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/json-iterator/go v1.1.10
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/resty.v1 v1.12.0
+)
